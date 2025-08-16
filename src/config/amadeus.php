81d6de8a199387f7436ab26cbd62506230bc896a
AMINE BOUTRA <118748281+amine2344@users.noreply.github.com>
2025-08-17 01:22:30 +0200

    Update amadeus.php

--- src/config/amadeus.php
+++ src/config/amadeus.php
@@ -3,8 +3,8 @@
  PHP ShaBax/Amadeus Package for Amadeus Self-Service Atravel APIs
 */
 return [
-    'client_id'=>'YOUR CLIENT ID',
-    'client_secret'=>'YOUR CLIENT SECRET',
+    'client_id'=>'nA72mYtpO1mQGKDiNEvLAGen0kEsMEb4',
+    'client_secret'=>'yYCrCp3SGk2QRlE6',
     'grant_type'=>'client_credentials',
 	'sandbox'=>true,
     'test_link'=>'https://test.api.amadeus.com/',
